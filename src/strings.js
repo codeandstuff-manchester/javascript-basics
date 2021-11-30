d9f98111caeae49b05f596bd08be6d4a49e7d95b
July Moss <july.x.moss@gmail.com>
2021-11-30 00:21:15 +0000

    refactored strings.js

--- src/strings.js
+++ src/strings.js
@@ -1,38 +1,14 @@
-const sayHello = (word) => {
-  if (typeof word === 'string'){
-    return 'Hello, ' + word + '!';
-  }
-};
+const sayHello = (string) => `Hello, ${string}!`;
 
-const uppercase = (upperWord) => {
-  if (typeof upperWord === 'string'){
-    return upperWord.toUpperCase();
-  }
-};
+const uppercase = (upperWord) => upperWord.toUpperCase();
 
-const lowercase = (lowerWord) => {
-  if (typeof lowerWord === 'string'){
-    return lowerWord.toLowerCase();
-  }
-};
+const lowercase = (lowerWord) => lowerWord.toLowerCase();
 
-const countCharacters = (countWord) => {
-  if (typeof countWord === 'string'){
-    return countWord.length;
-  }
-};
+const countCharacters = (countWord) => countWord.length;
 
-const firstCharacter = (firstAlp) => {
-  if (typeof firstAlp === 'string'){
-    return firstAlp[0];
-  }
-};
+const firstCharacter = (firstAlp) => firstAlp[0];
 
-const firstCharacters = (string, n) => {
-  if (typeof string === 'string'){
-    return string.slice(0, n);
-  }
-};
+const firstCharacters = (string, n) => string.slice(0, n);
 
 module.exports = {
   sayHello,
